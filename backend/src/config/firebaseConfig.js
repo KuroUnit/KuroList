b3f748558668f72fc717383c51678ece1558b4b9
JandersonLSilva <janderson.user.com>
2025-07-01 18:04:37 -0300

    Adicionando o certificado via arquivo json no arquivo de configuração do firebase.

--- backend/src/config/firebaseConfig.js
+++ backend/src/config/firebaseConfig.js
@@ -2,13 +2,16 @@ import admin from 'firebase-admin';
 import {getFirestore} from 'firebase-admin/firestore';
 import dotenv from 'dotenv';
 
+import serviceAccount from './firebase-credentials.json' with { type: 'json' };
+
 dotenv.config();
+
 
 if (admin.apps.length === 0) {
   try {
     console.log('Inicializando o Firebase Admin SDK...');
     admin.initializeApp({
-      credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CREDENTIALS)),
+      credential: admin.credential.cert(serviceAccount),
     });
   } catch (error) {
     console.error('Ocorreu um erro de inicialização do administrador para conexão com o Firebase', error.stack);
