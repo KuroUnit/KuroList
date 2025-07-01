808711d629856bcdd97991a4ac94f3c17ceb2199
JandersonLSilva <janderson.user.com>
2025-07-01 18:06:20 -0300

    Adicionando validação dos dados enviados no body da rota de login.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,25 +1,43 @@
 import express, { response } from 'express';
 import { userModel } from '../models/userModel.js';
+import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
+router.get('/auth/login', (req, res) => {
+  res.status(200).json({ response: [{ msg: 'Rota de teste GET /auth/login funcionando.' }] });
+});
+
 // Rota para login do usuário.
-router.post('/auth/login', async (req, res, next) => {
-  try {
-    const { login, password } = req.body;
-    let user = await userModel.findByLogin(login);
+router.post('/auth/login', 
+   [
+    body('login').isEmail().withMessage('Login deve ser um e-mail válido.'),
+    body('password').isLength({ min: 6 }).withMessage('A senha deve ter no mínimo 6 caracteres.')
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { login, password } = req.body;
+      let user = await userModel.findByLogin(login);
 
-    if (user) { // Login
-      const isValid = await userModel.validatePassword(password, user.password);
-      if (!isValid) {
-        return res.status(401).json({ errors: [{ msg: 'Credenciais inválidas' }] });
-      }
-    } else return res.status(404).json({ errors: [{ msg: 'Usuário não encontrado.' }] });
+      if (user) { // Login no Banco de dados
+        const isValid = await userModel.validatePassword(password, user.password);
+        if (!isValid) {
+          return res.status(401).json({ errors: [{ msg: 'Credenciais inválidas' }] });
+        }
+      } else return res.status(404).json({ errors: [{ msg: 'Usuário não encontrado.' }] });
 
-    res.status(200).json({ response: [{ msg: 'Usuário logado com sucesso.' }] });
-  } catch (err) {
-    next(err);
-  }
+      const payload = { user: { id: user.id } };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+      res.status(200).json({ token });
+    } catch (err) {
+      next(err);
+    }
 });
 
 export default router;
